76019d2fac22ea3fd389c45daa4d151c89c9ac09
EC2 Default User <ec2-user@ip-172-31-36-60.ap-south-1.compute.internal>
2023-01-22 11:29:00 +0000

    added for loop

--- login.java
+++ login.java
@@ -1,5 +1,12 @@
 public class Login
 {
+       public static void main(string args[])
+       {
+              int i;
 
-
+              for(i=0;i<=10;i++)
+              {
+                      system.out.println("Number : " + i);
+              }
+       }
 }
